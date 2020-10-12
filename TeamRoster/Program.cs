ee9ac55fc0f0b6cf8e9b21538f0f054c5c31f036
cjallen1988 <cjallen1988@gmail.com>
2020-10-11 19:06:32 -0500

    finished studio 4 example

--- TeamRoster/Program.cs
+++ TeamRoster/Program.cs
@@ -40,9 +40,9 @@ namespace TeamRoster
             seattleStorm.Players.Add(mTuck);
             seattleStorm.Players.Add(sWhitcomb);
 
-            Console.WriteLine(seattleStorm);
+            Roster noTeam = new Roster("Players without a Team");
 
-            List<Player> playersWaitingForTeam = new List<Player>();
+            
 
             
 
@@ -59,34 +59,108 @@ namespace TeamRoster
                         break;
 
                     case 1: //view roster menu
-                        Console.WriteLine(seattleStorm.ToString());
+                        Console.WriteLine(seattleStorm);
                         break;
 
                     case 2: //edit roster menu
                         int editMenuSelection = 100;
                         while(editMenuSelection > 0)
                         {
-                            Console.WriteLine("Please choose edit menu selection: \n1-Create Player \n2-Add Player \n3-Remove Player \n0-Exit Edit Menu");
+                            Console.WriteLine("\nPlease choose edit menu selection: \n1-Create Player \n2-Add Player \n3-Remove Player \n0-Exit Edit Menu");
                             editMenuSelection = int.Parse(Console.ReadLine());
                             switch(editMenuSelection)
                             {
                                 case 0: //Exit Edit Menu
                                     break;
                                 case 1: //Create Player
+                                    Console.WriteLine("\nPlease enter new players first name:");
+                                    string newFirstName = Console.ReadLine();
+                                    Console.WriteLine("\nPlease enter new players last name:");
+                                    string newLastName = Console.ReadLine();
+                                    Console.WriteLine("\nPlease enter new players jersey number:");
+                                    int newJerseyNumber = int.Parse(Console.ReadLine());
+                                    Console.WriteLine("\nPlease enter new players position:");
+                                    string newPosition = Console.ReadLine();
+                                    noTeam.AddPlayer(new Player(newFirstName, newLastName, newJerseyNumber, newPosition));
                                     break;
+
                                 case 2: //Add Player
+                                    if(noTeam.Players.Count < 1) {
+                                        Console.WriteLine("\nPlease create a player before trying to add a player");
+                                        break;
+                                    }
+
+                                    Console.WriteLine("\nPlease choose the first number of the player you want to add:");
+                                    int i = 1;
+                                    foreach(Player individual in noTeam.Players)
+                                    {
+                                        Console.WriteLine(i + " - " + individual.ToString() + " - " + individual.Position);
+                                        i++;
+                                    }
+                                    Console.WriteLine("0-Exit");
+                                    Player selectedPlayer = noTeam.Players[int.Parse(Console.ReadLine()) - 1];
+                                    bool wasPlayerAddedToSeattle = seattleStorm.AddPlayer(selectedPlayer);
+                                    bool wasPlayerRemovedFromNoTeam = noTeam.RemovePlayer(selectedPlayer);
+                                    if(wasPlayerAddedToSeattle && wasPlayerRemovedFromNoTeam)
+                                    {
+                                        Console.WriteLine("\nPlayer was added");
+                                    } else
+                                    {
+                                        if(wasPlayerAddedToSeattle)
+                                        {
+                                            seattleStorm.RemovePlayer(selectedPlayer);
+                                        }
+                                        if(wasPlayerRemovedFromNoTeam)
+                                        {
+                                            noTeam.AddPlayer(selectedPlayer);
+                                        }
+                                        Console.WriteLine("\nOoops something went wrong. Try again!");
+                                    }
                                     break;
+
                                 case 3: //Remove Player
+                                    if(seattleStorm.Players.Count < 1)
+                                    {
+                                        Console.WriteLine("\nNo players to remove.");
+                                        break;
+                                    }
+
+                                    Console.WriteLine("\nPlease choose the first number of the player you want to remove:");
+                                    int j = 1;
+                                    foreach(Player individual in seattleStorm.Players)
+                                    {
+                                        Console.WriteLine(j + " - " + individual.ToString() + " - " + individual.Position);
+                                        j++;
+                                    }
+                                    Player selectedRemovePlayer = seattleStorm.Players[int.Parse(Console.ReadLine()) - 1];
+                                    bool wasPlayerRemovedFromSeattle = seattleStorm.RemovePlayer(selectedRemovePlayer);
+                                    bool wasPlayerAddedToNoTeam = noTeam.AddPlayer(selectedRemovePlayer);
+                                    if(wasPlayerRemovedFromSeattle && wasPlayerAddedToNoTeam)
+                                    {
+                                        Console.WriteLine("\nPlayer removed successfully!");
+                                    } else
+                                    {
+                                        if(wasPlayerRemovedFromSeattle)
+                                        {
+                                            seattleStorm.AddPlayer(selectedRemovePlayer);
+                                        }
+                                        if(wasPlayerAddedToNoTeam)
+                                        {
+                                            noTeam.RemovePlayer(selectedRemovePlayer);
+                                        }
+                                        Console.WriteLine("\nOoops. Something went wrong. Try again!");
+                                    }
                                     break;
+
                                 default:
-                                    Console.WriteLine("Invalid Input");
+                                    Console.WriteLine("\nInvalid Input");
                                     break;
                             }
                         }
                         break;
 
                     default:
-                        Console.WriteLine("Invalid Input");
+                        Console.WriteLine("\nInvalid Input");
                         break;
                 }
             }  
